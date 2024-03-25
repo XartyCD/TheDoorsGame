c23c36e5256d876a3b82144a742ddf8801588a14
XartyCD <Frexozagsap@yandex.ru>
2024-03-25 12:31:31 +0300

    new functional

--- app.js
+++ app.js
@@ -5,55 +5,67 @@ const door3 = document.getElementById("door3")
 const doors = document.querySelectorAll(".doors__item")
 const itog = document.getElementById("result")
 
+let globalLevelNum = 1
 const losePhrases = ["Ты зашел не в ту дверь", "Welcome to the GYM", "Зря ты туда полез", "В дверь постучали 8 раз...", "Ну, он чувствует", "Вам намазали одеялко", "Ой, ручка отпала", "Сомнительно, нуу оке-е-й", "Тут оказался зачет у Али"]
 
-function godDoor(level) {
-    if (level === 1) {
-        win()
-        return 0;
-    }
-
-    let dr = Math.floor(Math.random() * doors.length)
-    console.log(dr)
-    return dr
+function godDoor(level, sellectDoor) {
+    let generatedDoor = Math.floor(Math.random() * doors.length)
+    // if (level === 1) {
+    //     return [...doors].indexOf(sellectDoor)
+    // }
+    console.log(generatedDoor)
+    return generatedDoor
 }
 
-door1.onclick = () => {
-    if (doors[godDoor(levelNum.textContent)] === door1) {
+const clickDoor1 = door1.onclick = () => {
+    if (doors[godDoor(globalLevelNum, door1)] === door1) {
         win()
     } else {
         lose()
     }
 }
 
-door2.onclick = () => {
-    if (doors[godDoor(levelNum.textContent)] === door2) {
+const clickDoor2 = door2.onclick = () => {
+    if (doors[godDoor(globalLevelNum, door2)] === door2) {
         win()
     } else {
         lose()
     }
 }
 
-door3.onclick = () => {
-    if (doors[godDoor(levelNum.textContent)] === door3) {
+const clickDoor3 = door3.onclick = () => {
+    if (doors[godDoor(globalLevelNum, door3)] === door3) {
         win()
     } else {
         lose()
     }
 }
+
+document.addEventListener('keyup', function (event) {
+    console.log(event.code)
+    if (event.code == 'Digit1') {
+        clickDoor1()
+    } else if (event.code == 'Digit2') {
+        clickDoor2()
+    } else if (event.code == 'Digit3') {
+        clickDoor3()
+    }
+  })
 
 function win() {
     itog.style.display = "flex"
     itog.textContent = "Проходим дальше..."
     itog.style.backgroundColor = "rgba(100, 159, 4, 0.696)"
-    setTimeout(() => {itog.style.display = "none"}, 1000)
-    levelNum.textContent = String(+levelNum.textContent + 1)
+    setTimeout(() => {itog.style.display = "none"}, 800)
+    globalLevelNum += 1
+    levelNum.textContent = String(globalLevelNum)
 }
 
 function lose() {
     itog.style.display = "flex"
     itog.textContent = losePhrases[Math.floor(Math.random() * losePhrases.length)]
     itog.style.backgroundColor = "rgba(159, 4, 4, 0.696)"
-    setTimeout(() => {itog.style.display = "none"}, 2000)
-    levelNum.textContent = "1"
+    setTimeout(() => {itog.style.display = "none"}, 1000)
+    globalLevelNum = 1
+    levelNum.textContent = String(globalLevelNum)
 }
