c95bf55735eb31cf6572b9a696e308c16fdbac4e
XartyCD <Frexozagsap@yandex.ru>
2024-03-26 18:39:38 +0300

    version 0.2 – added keystroke door selection, switch to "functional approach" code improved generation security

--- app.js
+++ app.js
@@ -4,68 +4,105 @@ const door2 = document.getElementById("door2")
 const door3 = document.getElementById("door3")
 const doors = document.querySelectorAll(".doors__item")
 const itog = document.getElementById("result")
+const goodDoors = document.getElementById("info-good")
+const badDoors = document.getElementById("info-bad")
 
-let globalLevelNum = 1
 const losePhrases = ["Ты зашел не в ту дверь", "Welcome to the GYM", "Зря ты туда полез", "В дверь постучали 8 раз...", "Ну, он чувствует", "Вам намазали одеялко", "Ой, ручка отпала", "Сомнительно, нуу оке-е-й", "Тут оказался зачет у Али"]
 
-function godDoor(level, sellectDoor) {
-    let generatedDoor = Math.floor(Math.random() * doors.length)
-    // if (level === 1) {
-    //     return [...doors].indexOf(sellectDoor)
-    // }
-    console.log(generatedDoor)
-    return generatedDoor
-}
+godDoor() // Первоначальный запуск шайтан машины после загрузки страницы
 
-const clickDoor1 = door1.onclick = () => {
-    if (doors[godDoor(globalLevelNum, door1)] === door1) {
-        win()
-    } else {
-        lose()
+// Генерация победных дверей
+function godDoor(level=1) {
+    let winDoors = []
+    let copyDoors = [...doors]
+
+    if (level === 1) {
+        chooseDoor(level, copyDoors)
+
+    } else if (level >= 2 && level <= 99) {
+        copyDoors.splice(3, copyDoors.length) // Обрезаем массив до нужного кол-ва элементов (по уровню сложности)
+
+        let numDoor = copyDoors[Math.floor(Math.random() * copyDoors.length)]
+        winDoors.push(numDoor) // пушим в вин-массив элемент из основного
+        copyDoors.splice(copyDoors.indexOf(numDoor), 1) // удаляем из основного массива выигрышный элемент
+
+        chooseDoor(level, winDoors)
     }
 }
+// Генерация победных дверей
 
-const clickDoor2 = door2.onclick = () => {
-    if (doors[godDoor(globalLevelNum, door2)] === door2) {
-        win()
-    } else {
-        lose()
+// Обработка выбранной двери
+function chooseDoor(level, winDoors) {
+    const clickDoor1 = () => {
+        if (winDoors.includes(door1)) {
+            win(level);
+        } else {
+            lose(level);
+        }
+        removeEventListeners()
+    };
+    door1.addEventListener('click', clickDoor1);
+
+    const clickDoor2 = () => {
+        if (winDoors.includes(door2)) {
+            win(level);
+        } else {
+            lose(level);
+        }
+        removeEventListeners()
+    };
+    door2.addEventListener('click', clickDoor2);
+
+    const clickDoor3 = () => {
+        if (winDoors.includes(door3)) {
+            win(level);
+        } else {
+            lose(level);
+        }
+        removeEventListeners()
     }
+    door3.addEventListener('click', clickDoor3);
+
+    // Обработка нажатий клавиш (переброс на как если бы нажимали курсором)
+    const keyUpHandler = (event) => {
+        if (event.code === 'Digit1') {
+            clickDoor1();
+        } else if (event.code === 'Digit2') {
+            clickDoor2();
+        } else if (event.code === 'Digit3') {
+            clickDoor3();
+        }
+    };
+    document.addEventListener('keyup', keyUpHandler);
+
+    // Функция для удаления обработчиков событий
+    const removeEventListeners = () => {
+        document.removeEventListener('keyup', keyUpHandler);
+        door1.removeEventListener('click', clickDoor1);
+        door2.removeEventListener('click', clickDoor2);
+        door3.removeEventListener('click', clickDoor3);
+    };
 }
+// Обработка выбранной двери
 
-const clickDoor3 = door3.onclick = () => {
-    if (doors[godDoor(globalLevelNum, door3)] === door3) {
-        win()
-    } else {
-        lose()
-    }
-}
-
-document.addEventListener('keyup', function (event) {
-    console.log(event.code)
-    if (event.code == 'Digit1') {
-        clickDoor1()
-    } else if (event.code == 'Digit2') {
-        clickDoor2()
-    } else if (event.code == 'Digit3') {
-        clickDoor3()
-    }
-  })
-
-function win() {
+// Обработка победы/луза
+function win(level) {
     itog.style.display = "flex"
     itog.textContent = "Проходим дальше..."
     itog.style.backgroundColor = "rgba(100, 159, 4, 0.696)"
     setTimeout(() => {itog.style.display = "none"}, 800)
-    globalLevelNum += 1
-    levelNum.textContent = String(globalLevelNum)
+    level += 1
+    levelNum.textContent = String(level)
+    godDoor(level)
 }
 
-function lose() {
+function lose(level) {
     itog.style.display = "flex"
     itog.textContent = losePhrases[Math.floor(Math.random() * losePhrases.length)]
     itog.style.backgroundColor = "rgba(159, 4, 4, 0.696)"
     setTimeout(() => {itog.style.display = "none"}, 1000)
-    globalLevelNum = 1
-    levelNum.textContent = String(globalLevelNum)
+    level = 1
+    levelNum.textContent = String(level)
+    godDoor(level)
 }
+// Обработка победы/луза
