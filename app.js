efb6611019e900ace3e5c8a355c52d7bf10f9c92
XartyCD <Frexozagsap@yandex.ru>
2024-03-27 17:37:01 +0300

    added new levels and optimization

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const levelNum = document.getElementById("level__num")
 const door1 = document.getElementById("door1")
 const door2 = document.getElementById("door2")
 const door3 = document.getElementById("door3")
+const door4 = document.getElementById("door4")
+const door5 = document.getElementById("door5")
+const door6 = document.getElementById("door6")
 const doors = document.querySelectorAll(".doors__item")
 const itog = document.getElementById("result")
-const goodDoors = document.getElementById("info-good")
-const badDoors = document.getElementById("info-bad")
+const goodDoors = document.getElementById("count-good")
+const badDoors = document.getElementById("count-bad")
 
 const losePhrases = ["Ты зашел не в ту дверь", "Welcome to the GYM", "Зря ты туда полез", "В дверь постучали 8 раз...", "Ну, он чувствует", "Вам намазали одеялко", "Ой, ручка отпала", "Сомнительно, нуу оке-е-й", "Тут оказался зачет у Али"]
 
@@ -15,24 +18,60 @@ godDoor() // Первоначальный запуск шайтан машины
 function godDoor(level=1) {
     let winDoors = []
     let copyDoors = [...doors]
+    doors.forEach(elem => elem.style.display = "none")
 
-    if (level === 1) {
-        chooseDoor(level, copyDoors)
+    if (level <= 5) {
 
-    } else if (level >= 2 && level <= 99) {
-        copyDoors.splice(3, copyDoors.length) // Обрезаем массив до нужного кол-ва элементов (по уровню сложности)
+        copyDoors.splice(5, 1) // Обрезаем массив до нужного кол-ва элементов (по уровню сложности)
+        clickableDoors = [...copyDoors]
+        copyDoors.forEach(elem => elem.style.display = "block")
 
-        let numDoor = copyDoors[Math.floor(Math.random() * copyDoors.length)]
-        winDoors.push(numDoor) // пушим в вин-массив элемент из основного
-        copyDoors.splice(copyDoors.indexOf(numDoor), 1) // удаляем из основного массива выигрышный элемент
+        if (level === 1) {
+            chooseDoor(level, winDoors = [...copyDoors], clickableDoors) // Возвращает в winDoors все элементы
+            copyDoors = []
+    
+        } else if (level <= 5) {
+            while (winDoors.length < 3) {
+                let numDoor = copyDoors[Math.floor(Math.random() * copyDoors.length)]
+                winDoors.push(numDoor) // пушим в вин-массив элемент из основного
+                copyDoors.splice(copyDoors.indexOf(numDoor), 1) // удаляем из основного массива выигрышный элемент
+            }
+            chooseDoor(level, winDoors, clickableDoors)
+        }
+    } else if (level >= 6 && level <= 10) {
+        
+        copyDoors.splice(4, 2) // Обрезаем массив до нужного кол-ва элементов (по уровню сложности)
+        clickableDoors = [...copyDoors]
+        copyDoors.forEach(elem => elem.style.display = "block")
 
-        chooseDoor(level, winDoors)
+        if (level <= 9) {
+            while (winDoors.length < 2) {
+                let numDoor = copyDoors[Math.floor(Math.random() * copyDoors.length)]
+                winDoors.push(numDoor) // пушим в вин-массив элемент из основного
+                copyDoors.splice(copyDoors.indexOf(numDoor), 1) // удаляем из основного массива выигрышный элемент
+            }
+            chooseDoor(level, winDoors, clickableDoors)
+        } else if (level === 10) {
+            while (winDoors.length < 1) {
+                let numDoor = copyDoors[Math.floor(Math.random() * copyDoors.length)]
+                winDoors.push(numDoor) // пушим в вин-массив элемент из основного
+                copyDoors.splice(copyDoors.indexOf(numDoor), 1) // удаляем из основного массива выигрышный элемент
+            }
+            chooseDoor(level, winDoors, clickableDoors)
+        }
     }
+    
+    console.log(copyDoors)
+    console.log(winDoors)
+    
+    // Вывод кол-ва победных/лузовых дверей
+    goodDoors.textContent = winDoors.length
+    badDoors.textContent = copyDoors.length
 }
 // Генерация победных дверей
 
 // Обработка выбранной двери
-function chooseDoor(level, winDoors) {
+function chooseDoor(level, winDoors, clickableDoors) {
     const clickDoor1 = () => {
         if (winDoors.includes(door1)) {
             win(level);
@@ -63,14 +102,51 @@ function chooseDoor(level, winDoors) {
     }
     door3.addEventListener('click', clickDoor3);
 
+    const clickDoor4 = () => {
+        if (winDoors.includes(door4)) {
+            win(level);
+        } else {
+            lose(level);
+        }
+        removeEventListeners()
+    }
+    door4.addEventListener('click', clickDoor4);
+
+    const clickDoor5 = () => {
+        if (winDoors.includes(door5)) {
+            win(level);
+        } else {
+            lose(level);
+        }
+        removeEventListeners()
+    }
+    door5.addEventListener('click', clickDoor5);
+
+    const clickDoor6 = () => {
+        if (winDoors.includes(door6)) {
+            win(level);
+        } else {
+            lose(level);
+        }
+        removeEventListeners()
+    }
+    door6.addEventListener('click', clickDoor6);
+
+
     // Обработка нажатий клавиш (переброс на как если бы нажимали курсором)
     const keyUpHandler = (event) => {
-        if (event.code === 'Digit1') {
+        if (event.code === 'Digit1' && clickableDoors.includes(door1)) {
             clickDoor1();
-        } else if (event.code === 'Digit2') {
+        } else if (event.code === 'Digit2' && clickableDoors.includes(door2)) {
             clickDoor2();
-        } else if (event.code === 'Digit3') {
+        } else if (event.code === 'Digit3' && clickableDoors.includes(door3)) {
             clickDoor3();
+        } else if (event.code === 'Digit4' && clickableDoors.includes(door4)) {
+            clickDoor4();
+        } else if (event.code === 'Digit5' && clickableDoors.includes(door5)) {
+            clickDoor5();
+        } else if (event.code === 'Digit6' && clickableDoors.includes(door6)) {
+            clickDoor6();
         }
     };
     document.addEventListener('keyup', keyUpHandler);
@@ -81,6 +157,9 @@ function chooseDoor(level, winDoors) {
         door1.removeEventListener('click', clickDoor1);
         door2.removeEventListener('click', clickDoor2);
         door3.removeEventListener('click', clickDoor3);
+        door4.removeEventListener('click', clickDoor4);
+        door5.removeEventListener('click', clickDoor5);
+        door6.removeEventListener('click', clickDoor6);
     };
 }
 // Обработка выбранной двери
